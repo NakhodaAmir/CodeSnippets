8cb20f5e7c5fe4c7641bf35fc6be7a00eb821d6b
Amir Nakhoda <107838446+NakhodaAmir@users.noreply.github.com>
2023-02-04 00:10:18 +0800

    Create Enumeration.cs

---
+++ C# Snippets/Enumeration.cs
@@ -0,0 +1,39 @@
+abstract public class Enumeration : System.IComparable
+        {
+            #region Properties
+            public int Id { get; private set; }
+            public string Name { get; set; }
+            #endregion
+
+            #region Static Variables
+            private static int id = 0;
+            #endregion
+
+            #region Constructor
+            protected Enumeration(string name)
+            {
+                Name = name;
+
+                Id = id++;
+            }
+            #endregion
+
+            #region Public Methods
+            public override string ToString() => Name;
+
+            public override bool Equals(object obj)
+            {
+                if (!(obj is Enumeration otherValue)) return false;
+
+                var typeMatches = GetType().Equals(otherValue.GetType());
+                var idMathches = Id.Equals(otherValue.Id);
+
+                return typeMatches && idMathches;
+            }
+
+            public int CompareTo(object obj) => Id.CompareTo(((Enumeration)obj).Id);
+
+            public override int GetHashCode() => Id.GetHashCode();
+            #endregion
+        }
+    }
