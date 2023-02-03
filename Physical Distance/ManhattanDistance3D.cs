5f5645c87f27a3bed7acac0f2378dc2c8b0a4f80
Amir Nakhoda <107838446+NakhodaAmir@users.noreply.github.com>
2023-02-03 19:50:13 +0800

    Create ManhattanDistance3D.cs

---
+++ Physical Distance/ManhattanDistance3D.cs
@@ -0,0 +1,7 @@
+//Pseudocode
+//Points p and q are represented as Vector3 variables where (p1, p2, p3) and (q1, q2, q3) = (p.x, p.y, p.z)
+//and (q.x, q.y, q.z) respectively.
+public float Distance(Vector3 p, Vector3 q)
+{
+	return  Math.Abs(p.x - q.x) + Math.Abs(p.y - q.y) + Math.Abs(p.z - q.z);
+}
