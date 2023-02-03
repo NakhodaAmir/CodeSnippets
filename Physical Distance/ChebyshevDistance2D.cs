7dc188731b2871af519722467266d1ced4cb1e40
Amir Nakhoda <107838446+NakhodaAmir@users.noreply.github.com>
2023-02-03 19:50:57 +0800

    Create ChebyshevDistance2D.cs

---
+++ Physical Distance/ChebyshevDistance2D.cs
@@ -0,0 +1,6 @@
+//Pseudocode
+//Points p and q are represented as Vector2 variables where (p1, p2) and (q1, q2) = (p.x, p.y) and (q.x, q.y) respectively.
+public float Distance(Vector2 p, Vector2 q)
+{
+	return  Math.Max(Math.Abs(p.x - q.x), Math.Abs(p.y - q.y));
+}
